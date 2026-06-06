03bb58bc0682a7932aba959ff0947e90135dbba5
Its-Suraj <20244098@g.deu.ac.kr>
2026-06-06 23:42:35 +0900

    Update Person.cpp
    Structure updated...

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,44 +1,34 @@
 #include "Person.h"
-
 #include "HospitalException.h"
 #include "TextUtils.h"
-
 using namespace std;
 
-Person::Person(int id, const string &name, const string &phone)
-    : id(id)
-{
-    if (id <= 0)
-    {
+Person::Person(int id, const string &name, const string &phone) : id(id) {
+    if (id <= 0) {
         throw ValidationException("Person id must be positive.");
     }
     setName(name);
     setPhone(phone);
 }
 
-int Person::getId() const
-{
+int Person::getId() const {
     return id;
 }
 
-const string &Person::getName() const
-{
+const string &Person::getName() const {
     return name;
 }
 
-const string &Person::getPhone() const
-{
+const string &Person::getPhone() const {
     return phone;
 }
 
-void Person::setName(const string &newName)
-{
+void Person::setName(const string &newName) {
     text::requireField(newName, "Name");
     name = text::trim(newName);
 }
 
-void Person::setPhone(const string &newPhone)
-{
+void Person::setPhone(const string &newPhone) {
     text::requireField(newPhone, "Phone");
     phone = text::trim(newPhone);
 }
